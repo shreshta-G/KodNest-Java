cfb29e34a82e3df5377e2b01e42d04ba6c8d7b92
shreshta <shreshtaspoorthi27!gmail.com>
2026-08-04 13:02:14 +0530

    practicereport

---
+++ PracticeSession/practicereport.java
@@ -0,0 +1,16 @@
+
+public class practicereport {
+
+    public static void main(String[] args) {
+
+        double solvedProblems = 47;
+        int wholePercentage = (int) 86.75;
+        double averagePerDay = solvedProblems / 5;
+        boolean isTargetReached = averagePerDay >= 9.0;
+
+        System.out.println("Report Value: " + solvedProblems);
+        System.out.println("Whole Percentage: " + wholePercentage);
+        System.out.println("Average Per Day: " + averagePerDay);
+        System.out.println("Daily Target Reached: " + isTargetReached);
+    }
+}

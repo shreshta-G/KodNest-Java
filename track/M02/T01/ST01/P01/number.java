52f8c3157cda7da06e4d608b3bcb9ecf3ecd75bb
shreshta <shreshtaspoorthi27!gmail.com>
2026-08-04 12:28:31 +0530

    number

---
+++ track/M02/T01/ST01/P01/number.java
@@ -0,0 +1,16 @@
+
+class number {
+
+    public static void main(String[] args) {
+        int num = 10;
+        if (num > 0) {
+            System.out.println("positive");
+        } else {
+            System.out.println("negative");
+        }
+        if (num == 0) {
+            System.out.println("zero");
+        }
+
+    }
+}

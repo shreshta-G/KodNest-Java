9d2f7cdf4bdea00bde1123172ece11fe354bf78e
shreshta <shreshtaspoorthi27!gmail.com>
2026-08-06 12:38:19 +0530

    Break

---
+++ track/M02/break1.java
@@ -0,0 +1,15 @@
+package KODNEST;
+
+public class Break1 {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            if (i == 6) {
+                break;
+            }
+            System.out.println(i);
+        }
+
+    }
+
+}

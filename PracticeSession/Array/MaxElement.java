1e2669fa694baad4917f9f1a965ddc11ee2a9843
shreshta <shreshtaspoorthi27!gmail.com>
2026-08-18 11:30:55 +0530

    Max

---
+++ PracticeSession/Array/MaxElement.java
@@ -0,0 +1,27 @@
+package Array;
+
+import java.util.Scanner;
+
+public class MaxElement {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        int arr[]=new int[5];
+        System.out.println("Enter the elements : ");
+        for(int i=0; i<=arr.length-1;i++) {
+            arr[i]=scan.nextInt();
+        }
+
+        System.out.println("Array elements are :");
+        for(int i =0;i<=arr.length-1;i++){
+            System.out.println(arr[i] + " ");
+        }
+        int max=arr[0];
+        for (int i = 0; i <= arr.length - 1; i++) {
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        } 
+        System.out.println("Max elements is : " + max);       
+    }
+    
+}

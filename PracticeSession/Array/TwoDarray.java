226ae799102bec48f0cd1b3478c2d53003b34a7b
shreshta <shreshtaspoorthi27!gmail.com>
2026-08-19 11:57:52 +0530

    2D Array

---
+++ PracticeSession/Array/TwoDarray.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+
+
+public class TwoDarray {
+    public static void main(String[] args) {
+        int a[][]=new int[3][5];
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Enter the array elements: ");
+        for(int i=0;i<=2;i++){
+            for(int j=0;j<=4;j++){
+                a[i][j]=scan.nextInt();
+            }
+        }
+        System.out.println("Enter the array elements: ");
+        for(int i=0;i<=2;i++){
+            for(int j=0;j<=4;j++){
+            System.out.print(a[i][j] + " ");
+            }
+            System.out.println();
+
+        
+    }
+
+    
+}}
